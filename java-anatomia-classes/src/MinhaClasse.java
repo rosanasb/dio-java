c5bed13f74735044405d5b403142e9f0581b085b
Rosana Schreiner Budant <rosanasbudant@outlook.com>
2024-05-03 12:22:51 -0300

    Anatomia da Classe - Aula 03

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -5,6 +5,15 @@ public class MinhaClasse {
         //Anatomia da Classe - Aula 02
         final String BR = "Brasil";
         System.out.println("My Country: " + BR); 
-
+        //Anatomia da Classe - Aula 03
+        String primeiroNome = "Rosana";
+        String segundoNome = "Silva";
+        int anoNascimento = 1988;
+        System.out.printf("\tNome: %s\n\tAno de Nascimento: %d\n", primeiroNome, anoNascimento);
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
