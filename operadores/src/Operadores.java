3bebd50c53316df5a1f65bb5a7afee241f48cbde
Rosana Schreiner Budant <rosanasbudant@outlook.com>
2024-05-07 15:39:21 -0300

    Testando + em Strings

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -2,5 +2,22 @@ public class Operadores {
     public static void main(String[] args) throws Exception {
         String nomeCompleto = "LINGUAGEM" + "JAVA";
         System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
     }
 }
