84130b058c603c108b5e9d136cf2a931d73fea76
Rosana Schreiner Budant <rosanasbudant@outlook.com>
2024-05-07 16:01:57 -0300

    Operador ternario

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -48,5 +48,21 @@ public class Operadores {
         System.out.println(! variavel);
         variavel = !variavel;
         System.out.println(variavel);
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+        String resultado;
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+        System.out.println(resultado);
+
+        //mesmo, mas com operador ternario
+        String resultado1 = (a==b) ? "verdadeiro" : "falso";
+
+        System.out.println(resultado1);
     }
 }
