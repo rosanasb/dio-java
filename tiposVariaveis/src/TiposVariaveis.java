058f2c99ee597058085821f3f46264262a268da4
Rosana Schreiner Budant <rosanasbudant@outlook.com>
2024-05-07 15:28:33 -0300

    Tipos 1

--- tiposVariaveis/src/TiposVariaveis.java
+++ tiposVariaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //erro, ou muda par int ou faz um cast
+
+        //valores final CAIXA ALTA
+        final double VALOR_DE_PI = 3.14;
     }
 }
