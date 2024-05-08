6dfb59fa220781156bdc6d3b499bfcac9ff22b3c
Rosana Schreiner Budant <rosanasbudant@outlook.com>
2024-05-08 16:04:00 -0300

    Exercicio Metodos

--- metodos/src/Usuario.java
+++ metodos/src/Usuario.java
@@ -35,8 +35,25 @@
 	//ou
 	public void gravar(Cliente cliente){}
 } */
-public class Metodos {
+public class Usuario {
     public static void main(String[] args) throws Exception {
-        
+		SmartTv smartTv = new SmartTv();
+
+		System.out.println("TV ligada? " + smartTv.ligada);
+		System.out.println("Canal Atual: " + smartTv.canal);
+		System.out.println("Volume Atual: " + smartTv.volume);
+
+		smartTv.ligar();
+		System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+
+		smartTv.diminuirVolume();
+		smartTv.diminuirVolume();
+		smartTv.diminuirVolume();
+	
+		smartTv.aumentarVolume();
+		
+		smartTv.mudarCanal(639);
+		System.out.println("Canal Atual: " + smartTv.canal);
+		
     }
 }
