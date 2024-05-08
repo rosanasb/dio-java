3cf0975d422d6830aedf2ad50e0ae65419beae76
Rosana Schreiner Budant <rosanasbudant@outlook.com>
2024-05-08 16:28:46 -0300

    Comentarios

---
+++ calculadora/src/Comentario.java
@@ -0,0 +1,12 @@
+public class Comentario {
+    public static void main(String[] args){
+        //Comentario de 1 linha
+
+        /*Comentario 
+         * mais 
+         * detalhado,
+         * varias 
+         * linhas
+         */
+    }
+}
