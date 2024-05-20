769618f24a16fbf17bb232189b5e9613f2114c69
Rosana Schreiner Budant <rosanasbudant@outlook.com>
2024-05-20 15:32:05 -0300

    Exemplo de visibilidade de recursos

---
+++ exemplo-lanchonete/src/lanchonete/Estabelecimento.java
@@ -0,0 +1,58 @@
+package lanchonete;
+
+import lanchonete.area.cliente.Cliente;
+import lanchonete.atendimento.Atendente;
+//import lanchonete.atendimento.cozinha.Almoxarife;
+import lanchonete.atendimento.cozinha.Cozinheiro;
+
+public class Estabelecimento {
+	public static void main(String[] args) {
+		Cozinheiro cozinheiro = new Cozinheiro();
+		//ações que não precisam estarem disponíveis para toda a aplicação, private
+		//cozinheiro.lavarIngredientes();
+		//cozinheiro.baterVitaminaLiquidificador();
+		//cozinheiro.selecionarIngredientesVitamina();
+		//cozinheiro.prepararLanche();
+		//cozinheiro.prepararVitamina();
+		//cozinheiro.prepararVitamina();
+		
+		//ações que estabelecimento precisa ter ciência
+		cozinheiro.adicionarSucoNoBalcao();;
+		cozinheiro.adicionarLancheNoBalcao();
+		cozinheiro.adicionarComboNoBalcao();
+		
+		//Almoxarife almoxarife = new Almoxarife();
+        //em necessidade dela aqui
+		//ações que não precisam estarem disponíveis para toda a aplicação
+		//almoxarife.controlarEntrada();
+		//almoxarife.controlarSaida();
+		//ação que somente o seu pacote cozinha precisa conhecer (default)
+		//almoxarife.entregarIngredientes();
+		//almoxarife.trocarGas();
+        //deixam de ser disponibilizadas pois estao no default e so podem ser vistos dentro do pacote
+		
+		Atendente atendente = new Atendente();
+		// private atendente.pegarLancheCozinha();
+		atendente.receberPagamento();
+		atendente.servindoMesa();
+		//ação que somente o seu pacote cozinha precisa conhecer (default)
+		//atendente.trocarGas();
+		
+		Cliente cliente = new Cliente();
+		cliente.escolherLanche();
+		cliente.fazerPedido();
+		cliente.pagarConta();
+		
+		//não deveria, mas o estabelecimento
+		//ainda não definiu normas de atendimento
+		//cliente.pegarPedidoBalcao();
+		
+		//esta ação é muito sigilosa, qua tal ser privada ?
+		//cliente.consultarSaldoAplicativo();
+		
+		//já pensou os clientes ouvindo que o gás acabou ?
+		//cozinheiro.pedirParaTrocarGas(atendente);
+		//cozinheiro.pedirParaTrocarGas(almoxarife);
+		
+	}
+}
