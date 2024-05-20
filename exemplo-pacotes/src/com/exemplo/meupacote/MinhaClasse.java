0c4a1b25798e490adb9463feafff86404e90608a
Rosana Schreiner Budant <rosanasbudant@outlook.com>
2024-05-20 14:40:28 -0300

    criacao de package

---
+++ exemplo-pacotes/src/com/exemplo/meupacote/MinhaClasse.java
@@ -0,0 +1,9 @@
+package com.exemplo.meupacote;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Hello, package!");
+        //cria normal o arquivo, adiciona o package acima e no quick fix move para o package
+    }
+}
+
