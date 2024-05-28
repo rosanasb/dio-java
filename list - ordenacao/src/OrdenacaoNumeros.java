daf68c96489a6cf239352eac771ff10187604dfb
Rosana Schreiner Budant <rosanasbudant@outlook.com>
2024-05-28 16:20:31 -0300

    ordenacao na lista

---
+++ list - ordenacao/src/OrdenacaoNumeros.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+    private List<Integer> numerosList;
+
+    public OrdenacaoNumeros() {
+        this.numerosList = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        this.numerosList.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente() {
+        List<Integer> numerosAscendente = new ArrayList<>(this.numerosList);
+        if (!numerosList.isEmpty()) {
+            Collections.sort(numerosAscendente);
+            return numerosAscendente;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public List<Integer> ordenarDescendente() {
+        List<Integer> numerosAscendente = new ArrayList<>(this.numerosList);
+        if (!numerosList.isEmpty()) {
+            numerosAscendente.sort(Collections.reverseOrder());
+            return numerosAscendente;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirNumeros() {
+        if (!numerosList.isEmpty()) {
+            System.out.println(this.numerosList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+    public static void main(String[] args) {
+    
+        OrdenacaoNumeros numeros = new OrdenacaoNumeros();
+        //adicionando numeros na lista
+        numeros.adicionarNumero(2);
+        numeros.adicionarNumero(5);
+        numeros.adicionarNumero(4);
+        numeros.adicionarNumero(1);
+        numeros.adicionarNumero(99);
+
+        System.out.println("Numeros na ordem da lista");
+        numeros.exibirNumeros();
+
+        System.out.println(" --------------------------- ");
+
+        System.out.println("Numeros em ordem crescente");
+        System.out.println(numeros.ordenarAscendente());
+
+        System.out.println(" --------------------------- ");
+
+        System.out.println("Numeros em ordem decrescente");
+        System.out.println(numeros.ordenarDescendente());
+
+        System.out.println(" --------------------------- ");
+    }
+}
