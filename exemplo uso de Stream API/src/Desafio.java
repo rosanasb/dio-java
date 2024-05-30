9a4b00782a15a4c001f768e688c9424f11a0394c
Rosana Schreiner Budant <rosanasbudant@outlook.com>
2024-05-30 15:29:22 -0300

    Exemplos de Stream API com Java

---
+++ exemplo uso de Stream API/src/Desafio.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Desafio {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> numerosOrdenados = numeros.stream()
+                                                .sorted()
+                                                .collect(Collectors.toList());
+
+        numeros.stream()
+            .sorted()
+            .forEach(System.out::println);
+    }
+}
